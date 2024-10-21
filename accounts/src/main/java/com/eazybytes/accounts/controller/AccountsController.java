96a65b91f47bf74e76f69f6f1a3309f8b4bb1a0d
shaik shahinsha <shahinsha.skh@gmail.com>
2024-10-21 19:10:50 +0530

    added ratelimmtter and retry pattern to the microservices

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -195,11 +196,20 @@ public class AccountsController {
             )
     }
     )
+    @Retry(name="getBuildInfo",fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
     public ResponseEntity<String> getBuildInfo() {
         return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(buildVersion);
+    }
+
+
+//    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwble) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("17");
     }
 
     @Operation(
--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -1,10 +1,15 @@
 package com.eazybytes.gatewayserver;
 
+import org.bouncycastle.oer.its.ieee1609dot2.basetypes.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
@@ -28,16 +33,31 @@ public class GatewayserverApplication {
 					.route(p -> p
 							.path("/eazybank/loans/**")
 							.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
-									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-							.uri("lb://LOANS"))
+									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+									.retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+											.setBackoff(java.time.Duration.ofMillis(100), java.time.Duration.ofMillis(1000),2,true)))
+
+								.uri("lb://LOANS"))
 					.route(p -> p
 							.path("/eazybank/cards/**")
 							.filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
-									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+									.requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(useKeyResolver())))
 							.uri("lb://CARDS")).build();
 
 
 	}
 
+
+	@Bean
+	public RedisRateLimiter redisRateLimiter(){
+		return new RedisRateLimiter(1,1,1);
+	}
+
+	@Bean
+	KeyResolver useKeyResolver(){
+		return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("name")).defaultIfEmpty("anonymous");
+
+	}
 
 }
